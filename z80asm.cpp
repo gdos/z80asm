311b3c6b0f440ff5c1f0497e49a990e204d74449
Paulo Custodio <pauloscustodio@gmail.com>
2017-01-19 23:31:58 +0000

    Use the Assembler class in main()

--- z80asm.cpp
+++ z80asm.cpp
@@ -4,52 +4,22 @@
 // License: http://www.perlfoundation.org/artistic_license_2_0
 //-----------------------------------------------------------------------------
 
+#include "memcheck.h"
+#include "assembler.h"
+#include "options.h"
 #include <iostream>
 
-#include "icode.h"
-#include "error.h"
-#include "source.h"
-#include "scanner.h"
-#include "filesystem/path.h"
-#include "z80asm_config.h"
+int main(int argc, char* argv[]) {
 
-bool assemble(const std::string& input_filename) {
-    // open source
-    SourceStack source;
-    if (!source.open(input_filename)) {
-        fatal_open_file(input_filename);
-        return false;
-    }
+	if (!opts.parse(argc, argv))
+		return 1;			// parse error
 
-    // create object
-    filesystem::path input_path(input_filename);
-    filesystem::path obj_path = input_path.replace_extension(OBJ_EXT);
-    ObjectFile obj(obj_path.str());
-
-    // create scanner
-    Scanner scan(&source, &obj);
-    if (!scan.parse())
-        return false;
-
-    // assembly OK, generate object file
-    obj.write();
-
-    return true;
-}
-
-int main(int argc, char* argv[])
-{
-    bool ok = true;
-
-    cout_version();
-
-    if (argc == 1)
-        cout_usage();
-    else {
-        for (int i = 1; i < argc; ++i) {
-            if (!assemble(argv[i]))
-                ok= false;
-        }
-    }
-    return ok ? 0 : 1;
+	Assembler as;
+	for (Options::iterator it = opts.begin_files(); it != opts.end_files(); ++it) {
+		as.assemble(*it);
+	}
+	if (as.good())
+		return 0;			// assemble OK
+	else
+		return 1;			// assemble error
 }
